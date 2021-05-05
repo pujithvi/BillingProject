10adb4d86d3d374a173afa4ac587d8e1901ec192
xDementation <83043275+xDementation@users.noreply.github.com>
2021-05-05 17:53:13 -0400

    Update main.py
    updates main.py for the incoming pull request

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from functions import *
+from logger import *
 from billClass import *
-from queryTest import *
-import datetime
 import os
-
-directory = os.getcwd()
-for file in os.scandir(directory):
-    if (file.path.endswith('Log.txt') and file.is_file()):
-        os.remove(file.path)
-
-sql_selectMostRecentBill = '''
-select bill_id from hs_ci_bill order by id desc
-'''
-cur.execute(sql_selectMostRecentBill)
-mostRecentBill_id = cur.fetchone()[0]
-bill_num = int(mostRecentBill_id.split('AS')[1].split('Y')[0]) + 1
+import shutil
 
 AGLCharge_dictionary = {}
 Rate_dictionary = {}
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# create log path
+log_path = os.path.join(dir_path, "log")
+try:
+    shutil.rmtree(log_path)
+except OSError as e:
+    pass
+os.mkdir(log_path)
+
+# create billOutput path
+billOutput_path = os.path.join(dir_path, "billOutput")
+try:
+    shutil.rmtree(billOutput_path)
+except OSError as e:
+    pass
+os.mkdir(billOutput_path)
+
+# print("Directory '% s' created" % log_path)
+# print("Directory '% s' created" % billOutput_path)
 
 cycCode = getCycCode()
 # print(cycCode)
@@ -27,53 +36,74 @@ print(accountsToProcess)
 
 for account in accountsToProcess:
 
-    print(account)
+    # print(account)
+    l_fileName = '%s\%sLog.txt', log_path, account
+    l_fileNameTwo = f'{log_path}\{account}.txt'
+    l = setup_logger(account, l_fileNameTwo)
 
-        # Need to do like this for every method
-    try:
-        billEndDates = retrieveBillEndDates(account)
-        logger(account, method = 'retrieveBillEndDates', successful=True)
-    except Exception as e:
-        logger(account, str(e), 'retrieveBillEndDates')
+    # Need to do like this for every method (done)
+    billEndDates = logThis(l, retrieveBillEndDates, (account,), {})
+    if billEndDates is None:
         continue
-   
     # print(billEndDates)
-    mostRecentDate = findMostRecentEndDate(billEndDates)
-    # print(mostRecentDate)
-    startDate = createStartDate(mostRecentDate, account)
-    #print('start date:', startDate)
 
-
-    try:
-        serviceAgreement = getSA(account)
-        # print(serviceAgreement)
-    except Exception as e:
-        print('No service agreement for account ' + account)
-        logger(account, str(e), 'getSA')
-        billOutput(account = account, fail=True, text = "No service agreement for this account." )
+    mostRecentDate = logThis(l, findMostRecentEndDate, (billEndDates,), {})
+    if mostRecentDate is None:
         continue
+    # print(mostRecentDate)
 
+    startDate = logThis(l, createStartDate, (mostRecentDate, account), {})
+    if startDate is None:
+        continue
+    # print('start date:', startDate)
 
-    servicePoint = getSP(serviceAgreement)
+    serviceAgreement = logThis(l, getSA, (account,), {})
+    if serviceAgreement is None:
+        billOutput(account=account, fail=True, text="No service agreement for this account.", path=billOutput_path)
+        continue
+    # print(serviceAgreement)
+
+    servicePoint = logThis(l, getSP, (serviceAgreement,), {})
+    if servicePoint is None:
+        billOutput(account=account, fail=True, text="No service point for this account.", path=billOutput_path)
+        continue
     # print(servicePoint)
-    meter = getMeter(servicePoint)
-    #print('meter:', meter)
-    initialDate, finalDate, gasUsage = getGasUsage(meter, startDate)
-    gasUsage = convertToTherms(gasUsage)
+
+    meter = logThis(l, getMeter, (servicePoint,), {})
+    if meter is None:
+        billOutput(account=account, fail=True, text="No meter for this account.", path=billOutput_path)
+        continue
+    # print('meter:', meter)
+
+    initialDate, finalDate, gasUsage = logThis(l, getGasUsage, (meter, startDate), {})
+    # if initialDate or finalDate or gasUsage is None:
+        # continue
+    # problem with the above statement: one of those is returning None
+    # print(initialDate)
+
+    gasUsage = logThis(l, convertToTherms, (gasUsage,), {})
     # print(gasUsage)
 
-    rateSchedule = getRateSchedule(account)
+    rateSchedule = logThis(l, getRateSchedule, (account,), {})
+    if rateSchedule is None:
+        continue
     # print(rateSchedule)
-    AGLCharge = getAGLFixedCharge(rateSchedule, AGLCharge_dictionary)
+
+    AGLCharge = logThis(l, getAGLFixedCharge, (rateSchedule, AGLCharge_dictionary), {})
+    if AGLCharge is None:
+        continue
     # print(AGLCharge)
-    usageCharge = calculateGasCharge(rateSchedule, gasUsage, Rate_dictionary)
+
+    usageCharge = logThis(l, calculateGasCharge, (rateSchedule, gasUsage, Rate_dictionary), {})
+    if usageCharge is None:
+        continue
     # print(usageCharge)
 
     totalCost = AGLCharge + usageCharge
  
-    print(totalCost)
-    #Could create a bill object and go from there
+    # print(totalCost)
+    # Could create a bill object and go from there
     # finish this: bill = Bill(bill_id='SFGAS' + str(bill_num) + 'Y21', )
-    billOutput(account, initialDate, finalDate, gasUsage, AGLCharge, usageCharge)
 
-
+    billOutput(account, initialDate, finalDate, gasUsage, AGLCharge, usageCharge, path=billOutput_path)
+    # Note: billOutput methods aren't logged yet in the case that they get changed in the future
